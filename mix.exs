b5e78fd7350f290d30734f594a0bcae191d9258a
Michał Buszkiewicz <michal@curiosum.com>
2023-05-25 22:44:14 +0200

    Bump to 0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Permit.Phoenix.MixProject do
   def project do
     [
       app: :permit_phoenix,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
