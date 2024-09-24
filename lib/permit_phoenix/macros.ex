afbc0bbd55e1c704e014d155b58aa980cba90244
Darren Gegantino <darrengegantino@gmail.com>
2024-09-24 21:12:25 +0800

    Complete Permit.Types module name in macro

--- lib/permit_phoenix/macros.ex
+++ lib/permit_phoenix/macros.ex
@@ -4,8 +4,9 @@ defmodule PermitPhoenix.Macros do
   defmacro permit_ecto_callbacks do
     if :ok == Application.ensure_loaded(:permit_ecto) do
       quote do
-        @callback base_query(Types.resolution_context()) :: Ecto.Query.t()
-        @callback finalize_query(Ecto.Query.t(), Types.resolution_context()) :: Ecto.Query.t()
+        @callback base_query(Permit.Types.resolution_context()) :: Ecto.Query.t()
+        @callback finalize_query(Ecto.Query.t(), Permit.Types.resolution_context()) ::
+                    Ecto.Query.t()
       end
     end
   end
